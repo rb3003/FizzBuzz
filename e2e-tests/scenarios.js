0233fde42f770f06f57996330c55ab604ed3a532
rb3003 <rballen@insight.rr.com>
2016-01-19 18:35:46 -0500

    Added textbox and e2e tests

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -19,24 +19,51 @@ describe('my app', function() {
 
 
     it('should render FizzBuzz when user navigates to /FizzBuzz', function() {
-      expect(element.all(by.css('[ng-FizzBuzz] p')).first().getText()).
+      expect(element.all(by.css('p')).first().getText()).
         toMatch(/partial for view 1/);
     });
 
+    it('should render Fizz when user enters 6', function() {
+      element(by.model('formModel.inputtedValue')).sendKeys('6');
+      var button = element(by.id('submit'));
+      button.click();
+      var elem = element(by.xpath("//label[contains(text(), 'Fizz')]"));
+
+      browser.wait(protractor.ExpectedConditions.presenceOf(elem), 5000);
+      expect(elem.isPresent()).toEqual(true);
+    });
+
+    it('should render Buzz when user enters 10', function() {
+      element(by.model('formModel.inputtedValue')).sendKeys('10');
+      var button = element(by.id('submit'));
+      button.click();
+      var elem = element(by.xpath("//label[contains(text(), 'Buzz')]"));
+
+      browser.wait(protractor.ExpectedConditions.presenceOf(elem), 5000);
+      expect(elem.isPresent()).toEqual(true);
+    });
+
+    it('should render FizzBuzz when user enters 15', function() {
+      element(by.model('formModel.inputtedValue')).sendKeys('15');
+      var button = element(by.id('submit'));
+      button.click();
+      var elem = element(by.xpath("//label[contains(text(), 'FizzBuzz')]"));
+
+      browser.wait(protractor.ExpectedConditions.presenceOf(elem), 5000);
+      expect(elem.isPresent()).toEqual(true);
+    });
+
+    it('should render 16 when user enters 16', function() {
+      element(by.model('formModel.inputtedValue')).sendKeys('16');
+      var button = element(by.id('submit'));
+      button.click();
+      var elem = element(by.xpath("//label[contains(text(), '16')]"));
+
+      browser.wait(protractor.ExpectedConditions.presenceOf(elem), 5000);
+      expect(elem.isPresent()).toEqual(true);
+    });
+
   });
 
 
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-FizzBuzz] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
